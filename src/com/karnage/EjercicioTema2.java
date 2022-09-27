184bff129308fd0ab3c9bf655749f93e54e25003
Karnage <muaddib83@gmail.com>
2022-09-27 10:26:33 +0200

    Initial commit

---
+++ src/com/karnage/EjercicioTema2.java
@@ -0,0 +1,18 @@
+package com.karnage;
+
+public class EjercicioTema2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(calculaIVA(28));
+
+
+    }
+
+    public static double calculaIVA(double precio){
+
+        return precio * 1.21;
+    }
+
+
+}
